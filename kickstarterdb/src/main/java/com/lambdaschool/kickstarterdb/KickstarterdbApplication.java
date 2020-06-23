50b5e129d5663a62c6219e03778768604013210d
strengthofthemountain <strengthofthemountain@gmail.com>
2020-06-23 10:49:16 -0400

    added files from current project

---
+++ kickstarterdb/src/main/java/com/lambdaschool/kickstarterdb/KickstarterdbApplication.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.kickstarterdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KickstarterdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KickstarterdbApplication.class, args);
+    }
+
+}
---
+++ kickstarterdb/src/test/java/com/lambdaschool/kickstarterdb/KickstarterdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.kickstarterdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KickstarterdbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
